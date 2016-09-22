de7495f00a9da030433a6ec3f9f39dd8aacd7f91
Martin Hermes <martin.hermes@sap.com>
2016-09-22 13:27:53 +0200

    Fixed websocked ip

--- chart/V_chart.controller.js
+++ chart/V_chart.controller.js
@@ -22,7 +22,7 @@ sap.ui.controller("chart.V_chart", {
 		that = this;
 		// Establish the WebSocket connection and set up event handlers
 		//this.webSocket = new WebSocket("ws://localhost:4567/echo/?selectedSensors=" + this.getSelectedSensors());
-		this.webSocket = new WebSocket("ws://52.59.224.66:8001/posco?selectedSensors=" + this.getSelectedSensors());
+		this.webSocket = new WebSocket("ws://52.59.116.20:8001/posco?selectedSensors=" + this.getSelectedSensors());
 		this.webSocket.onclose = function() {
 			//alert("WebSocket connection closed");
 		};
