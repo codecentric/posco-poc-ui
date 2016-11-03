84b77462b4b8f4a21360b2bb06738a4d0a997eb3
D025898 <martin.hermes@sap.com>
2016-11-03 09:51:01 +0100

    Fixed Date Format

--- table/list.controller.js
+++ table/list.controller.js
@@ -18,27 +18,27 @@ sap.ui.controller("table.list", {
 			this.getView().byId("starttime").setDateValue(new Date(start));
 		}
 		this.model.loadData("http://52.59.116.20:8001/metrics/cassandra?start=" + start +"&end=" + end, null, false);
-//		this.model.setData({
-//			"metrics":[  
-//			           {  
-//			              "start":1478080335,
-//			              "end":1478090335,
-//			              "count":10000,
-//			              "min":123.9,
-//			              "max":333.34,
-//			              "mean":239.03,
-//			              "stdev":50.2
-//			           },
-//			           {  
-//			              "start":1488080335,
-//			              "end":1488090335,
-//			              "count":10001,
-//			              "min":125.9,
-//			              "max":335.34,
-//			              "mean":235.03,
-//			              "stdev":49.2
-//			           } ]
-//		});
+//			this.model.setData({
+//				"metrics":[  
+//				           {  
+//				              "start":1478080335,
+//				              "end":1478090335,
+//				              "count":10000,
+//				              "min":123.9,
+//				              "max":333.34,
+//				              "mean":239.03,
+//				              "stdev":50.2
+//				           },
+//				           {  
+//				              "start":1488080335,
+//				              "end":1488090335,
+//				              "count":10001,
+//				              "min":125.9,
+//				              "max":335.34,
+//				              "mean":235.03,
+//				              "stdev":49.2
+//				           } ]
+//			});
 	},
 	
 	handleChange: function(oEvent) {
@@ -50,5 +50,18 @@ sap.ui.controller("table.list", {
 		} else {
 			oDTP.setValueState(sap.ui.core.ValueState.Error);
 		}
+	},
+	
+	getDate: function(value) {
+		  //return new Date(value);
+		var dateFormat = sap.ui.core.format.DateFormat.getDateInstance({pattern : "dd/MM/yyyy" });
+		var timeFormat = sap.ui.core.format.DateFormat.getTimeInstance({pattern: "KK:mm:ss a"});       
+		// timezoneOffset is in hours convert to milliseconds
+		var TZOffsetMs = new Date(0).getTimezoneOffset()*60*1000;
+		// format date and time to strings offsetting to GMT
+		var dateStr = dateFormat.format(new Date(value * 1000));
+		var timeStr = timeFormat.format(new Date(value * 1000));
+		return dateStr + " " + timeStr; 
 	}
+
 });
